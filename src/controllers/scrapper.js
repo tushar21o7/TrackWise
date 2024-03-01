806fc34e53130a52af545c4363db2d98286e4c9f
Tushar Mishra <tusharmishra2107@gmail.com>
2024-03-02 00:26:44 +0530

    update url

--- src/controllers/scrapper.js
+++ src/controllers/scrapper.js
@@ -35,7 +35,7 @@ export const getAllProducts = async (req, res) => {
   const url = `https://www.flipkart.com/search?q=${query}`;
 
   try {
-    const resp = await axios.get(url, {
+    const resp = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto", {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
